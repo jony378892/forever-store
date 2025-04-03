d8a487b743f41b5a2f34a748d6f1e270110c4566
Jony <34894127+jony378892@users.noreply.github.com>
2025-04-03 16:32:29 +0000

    Added admin folder

---
+++ Admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} alt="" />
+      <button class>Logout</button>
+    </div>
+  );
+};
+
+export default Navbar;
